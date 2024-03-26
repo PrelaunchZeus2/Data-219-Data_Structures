c101572f2a0a5124ea07bab0cc944ffe6db615be
John <jfsalmoniv@gmail.com>
2024-03-26 12:47:03 -0700

    Added Notes

--- Code Snippets/HashTables.py
+++ Code Snippets/HashTables.py
@@ -2,7 +2,6 @@ def hashFunction(x):
     """Example Hash function that returns the remainder of x divided by 2 as the index"""
     return x % 2
 
-#note the above wouldnt be very good because it only has 2 buckets, so it would be very easy to have a collision
 
 class HashTable(object):
     def put(self, key, value):
@@ -11,3 +10,9 @@ class HashTable(object):
     def get(key):
         pass
         
+#An ideal hash function does two things: 1. Large range of values 2. Evenly distributes values for all possible keys
+#Both of which will reduce the chances of colllisions.
+#If a hash value returned is larger than the array the hash functiioon can compress the value to fit the array.
+#The very top function would be terrible because it fails both of these criteria.
+#Its impossible to prove a perfect distriibution forr all possible keys but there exist some good "generic" hash functions that are good for most cases.
+
