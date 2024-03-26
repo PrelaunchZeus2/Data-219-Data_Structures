68109ddbf9932bdfd113902038f74bed33a06d30
John <jfsalmoniv@gmail.com>
2024-03-26 13:19:53 -0700

    added notes for heaps

---
+++ Code Snippets/Heaps.py
@@ -0,0 +1,6 @@
+# Heaps
+
+# Max heap has the property that for every node i other than the root
+# A[parent(i)] >= A[i]
+# A min heap has the property that for every node i other than the root
+# A[parent(i)] <= A[i]
