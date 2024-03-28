0b92882cc0074edaf871bc2352ec9e49c303cb20
John <jfsalmoniv@gmail.com>
2024-03-28 12:45:04 -0700

    added code with heapq module

--- Code Snippets/Heaps.py
+++ Code Snippets/Heaps.py
@@ -8,3 +8,24 @@
 
 #in a bst the maximum value is the right most value
 #in a max heap the maximum value is the root, vice versa for min heap
+
+import heapq
+
+#create a list 
+mylist = [1, 2, 3, 44, 23, 15, 7, 10]
+
+#Transform list into heap
+heapq.heapify(mylist)
+
+#Print the heap
+print ("Heap: ",(list(mylist)))
+
+#heappush() inserts values into the heap.
+heapq.heappush(mylist, 37)
+
+#heappull() removes values from the heap.
+heapq.heappop(mylist, 44)
+
+print("heap after changes:", (list(mylist)))
+
+
