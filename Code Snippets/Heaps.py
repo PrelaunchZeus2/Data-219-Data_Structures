80713ceba31156e5fa48ab4d062fb8a926c634c2
John <jfsalmoniv@gmail.com>
2024-04-19 15:00:18 -0700

    Finished Functions for each structure. Bug fixed code

--- Code Snippets/Heaps.py
+++ Code Snippets/Heaps.py
@@ -34,3 +34,37 @@ print("heap after changes:", (list(mylist)))
 #Right child return 2i + 1
 
 
+#W/O Heapify
+class Heap:
+    def __init__(self):
+        self.heap = []
+        
+    def build_heap(self, data):
+        """This function takes a set of nodes and build it into a heap using the heapify function.
+        @param data, the data to be built into a heap."""
+        self.heap = data[:]
+        for i in range(len(self.heap)// 2, -1, -1):
+            self.heapify(i)
+            
+    def heapify(self, i):
+        left = 2 * i + 1
+        right = 2 * i + 2
+        largest = i
+        if left < len(self.heap) and self.heap[left].score_difference > self.heap[largest].score_difference:
+            largest = left
+        if right < len(self.heap) and self.heap[right].score_difference > self.heap[largest].score_difference:
+            largest = right
+        if largest != i:
+            self.heap[i], self.heap[largest] = self.heap[largest], self.heap[i]
+            self.heapify(largest)
+            
+    def search(self, value):
+        """This function searchees for a node with a specific value in the heap, it will return the node if found or None if not found.
+        @param value, the value to bee search for in the heap
+        @return the nodee with the value or None."""
+        
+    def get_max(self):
+        """This function return the root node of the heap which is the maximum value."""
+        return self.heap[0]
+
+
